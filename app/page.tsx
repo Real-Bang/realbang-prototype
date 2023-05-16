f3a3c34672be3630801acb03c8fe5a567aff1d22
Kim Jeong-won <threedalpeng@gmail.com>
2023-05-16 13:35:46 +0900

    fix: temporarily disable /new page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,12 @@ export default function MainPage() {
     <main className="min-h-screen flex flex-col items-center justify-center">
       <Image src="/icon.png" width={500} height={500} alt="Logo" />
       <div className="fixed bottom-24 flex flex-col gap-4 w-2/3">
-        <button className="btn btn-primary btn-xl text-xl">
-          <Link href="/new">방 등록하기</Link>
+        <button
+          className="btn btn-primary btn-disabled btn-xl text-xl"
+          disabled
+        >
+          방 등록하기
+          {/* {<Link href="/new"></Link>} */}
         </button>
         <button className="btn btn-primary btn-xl text-xl">
           <Link href="/room">방 살펴보기</Link>
